b0483b6fcf65be0a705e3ee8948b8fcb94fa4b63
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-18 13:29:50 -0500

    added another author to the .json file and added a route for animal type

--- index.php
+++ index.php
@@ -20,5 +20,34 @@ $f3->route('GET /', function() {
     //echo $view->render('views/home.html');
 });
 
+//define an animal type route
+$f3->route('GET /@order', function($f3, $params) {
+    //echo '<h1>My pets</h1>';
+    //echo "<a href='order'>Order a pet</a>";
+    //$view = new View;
+    //echo $view->render('views/home.html');
+
+    switch ($params['order'])
+    {
+        case 'shark':
+            $sound = " nom nom"; break;
+
+        case 'dog':
+            $sound = " ruff ruff"; break;
+
+        case 'cat':
+            $sound = " meow"; break;
+
+        case 'wolf':
+            $sound = " aoooooo"; break;
+
+        case 'lion':
+            $sound = " roar"; break;
+
+        default:
+            $f3->error(404);
+    }
+});
+
 //Run fat free
 $f3->run();
